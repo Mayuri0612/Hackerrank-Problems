31a4138017b3322d8c528284f33fdc1d25ce9a4a
Mayuri0612 <500060750@stu.upes.ac.in>
2020-08-01 16:45:24 +0530

    Distinct count problem

---
+++ Hackerearth/DistinctCount.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n,x = map(int, input().strip().split())
+    arr = set(input().strip().split())
+    if(len(arr) == x):
+        print('Good')
+    elif(len(arr) < x):
+        print('Bad')
+    else:
+        print('Average')
