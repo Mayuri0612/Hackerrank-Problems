bf85d22008933cb678d7017e9d6d0b0959e9a061
Mayuri0612 <500060750@stu.upes.ac.in>
2020-08-19 21:42:12 +0530

    right bit shift

---
+++ GFG/CompanyWise/Commvault/1.py
@@ -0,0 +1,9 @@
+#Given 2 integers m, and n (decimal system), 
+#write a program to perform right shift on m , n times.
+def rightShift(m,n):
+    return (m >> n)
+
+n, m = map(int, input().split())
+print(rightShift(m,n))
+
+
